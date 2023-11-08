f1c419af819e862bcdb1fa42fd82605fd28fc05f
Alexandre Lima <alexandrevl@gmail.com>
2023-11-07 21:25:42 -0300

    Cleaning

--- tunnel_check.py
+++ tunnel_check.py
@@ -8,52 +8,19 @@ import time
 from datetime import datetime
 import subprocess
 from colorama import Fore, Style
-
+import platform
 
 def is_tunnel_working(port=8080):
     try:
-        # set up the SOCKS proxy and test the connection
         socks.set_default_proxy(socks.SOCKS5, "localhost", port)
         socket.socket = socks.socksocket
-        response = requests.get("http://google.com")
-        
-        # check the HTTP response status code
+        response = requests.get("http://144.22.144.218:8005/", timeout=2)
         if response.status_code == 200:
             return True
         else:
             print(f"Unexpected status code: {response.status_code}")
             return False
     except requests.exceptions.RequestException as e:
-        # log the exception
-        ##print(f"Error: {str(e)}")
-        return False
-
-def is_tunnel_working_old(port=8080):
-    s = socks.socksocket() # Same API as socket.socket in the standard lib
-    try: 
-        s.set_proxy(socks.SOCKS5, "localhost",port)
-        s.settimeout(2.0)
-
-        s.connect(("8.8.8.8", 53))
-
-        request = """GET / HTTP/1.1
-        Host: google.com
-        User-Agent: MyClient/1.0
-
-        """
-        # convert to bytes for sending over network
-        request_encoded = request.encode()
-
-        s.sendall(request_encoded)
-
-        # now receive the response
-        buffer_size = 4096
-        response = s.recv(buffer_size)
-
-        s.close()
-        return True
-    except Exception as e:
-        # print(e)
         return False
 
 def kill_tunnel(port):
@@ -70,59 +37,6 @@ def create_tunnel(port, ssh_user, ssh_host):
     result = subprocess.run(cmd, shell=True)
     return result.returncode == 0
 
-def main():
-    try:
-        last_ssid_check = False
-        port = 8080
-        ssh_user = 'ubuntu'
-        ssh_host = '144.22.144.218'
-        is_working = False
-        interval = 2  # Seconds
-
-        while True:
-            timestamp = datetime.now().strftime('%H:%M:%S')
-            
-            if not is_tunnel_working(port):
-                is_working = False
-                kill_tunnel(port)
-                print(f'{Fore.YELLOW}[{timestamp}] Tunnel not working. Creating a new one...{Style.RESET_ALL}')
-                if create_tunnel(port, ssh_user, ssh_host):
-                    change_proxy_location()
-                    print(f'{Fore.YELLOW}[{timestamp}] Tunnel created on port {port} with {ssh_user}@{ssh_host}{Style.RESET_ALL}')
-                    print(f'{Fore.YELLOW}[{timestamp}] Tunnel setting up. Wait...{Style.RESET_ALL}', end='\r')
-                    is_working  = True
-                else:
-                    #change_location('Automatic')
-                    print(f'{Fore.RED}[{timestamp}] Failed to create tunnel.{Style.RESET_ALL}')
-            else:
-                print(f'{Fore.GREEN}[{timestamp}] {Style.BRIGHT}Tunnel is working.({interval}s){Style.RESET_ALL}                   ', end='\r')
-                is_working = True
-
-            time.sleep(interval)  # Wait for X seconds before checking again
-    except KeyboardInterrupt:
-        timestamp = datetime.now().strftime('%H:%M:%S')
-        print('')
-        kill_tunnel(port)
-        change_automatic_location()
-        print(f'[{timestamp}] {Style.BRIGHT}Bye!{Style.RESET_ALL}')
-
-def check_wifi(ssid_target = "88200Wireless-d"):
-    COMMAND = "/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -I | grep SSID | tail -1 | awk '{print $2}'"
-    SSID = subprocess.check_output(COMMAND,shell = True).decode("utf-8").strip()
-    if (SSID == ssid_target):
-    # if (SSID == "hNet"):
-        return True
-    else:
-        return False
-
-def get_location(param_location):
-    COMMAND = "networksetup -getcurrentlocation"
-    LOCATION = subprocess.check_output(COMMAND,shell = True).decode("utf-8").strip()
-    if (LOCATION == param_location):
-        return True
-    else: 
-        return False
-
 def change_location(param_location):
     COMMAND = f'networksetup -switchtolocation {param_location}'
     RESULT = subprocess.check_output(COMMAND,shell = True).decode("utf-8").strip()
@@ -131,21 +45,43 @@ def change_location(param_location):
     else:
         return False    
 
-def change_proxy_location():
+def change_network_location(location):
     timestamp = datetime.now().strftime('%H:%M:%S')
-    changed = change_location('Proxy')
+    changed = change_location(location)
     if changed:
-        print(f'{Fore.GREEN}[{timestamp}] Location set to Proxy{Style.RESET_ALL}')
+        print(f'{Fore.GREEN}[{timestamp}] Location set to {location}{Style.RESET_ALL}')
     else:
-        print(f'{Fore.RED}[{timestamp}] Failed to change to Proxy location{Style.RESET_ALL}')
+        print(f'{Fore.RED}[{timestamp}] Failed to change to {location} location{Style.RESET_ALL}')
 
-def change_automatic_location():
-    timestamp = datetime.now().strftime('%H:%M:%S')
-    changed = change_location('Automatic')
-    if changed:
-        print(f'{Fore.GREEN}[{timestamp}] Location set to Automatic{Style.RESET_ALL}')
-    else:
-        print(f'{Fore.RED}[{timestamp}] Failed to change to Automatic location{Style.RESET_ALL}')
+def main():
+    try:
+        port = 8080
+        ssh_user = 'ubuntu'
+        ssh_host = '144.22.144.218'
+        interval = 3  # Seconds
+
+        while True:
+            timestamp = datetime.now().strftime('%H:%M:%S')
+            
+            if not is_tunnel_working(port):
+                kill_tunnel(port)
+                print(f'{Fore.YELLOW}[{timestamp}] Tunnel not working. Creating a new one...{Style.RESET_ALL}')
+                if create_tunnel(port, ssh_user, ssh_host):
+                    change_network_location('Proxy')
+                    print(f'{Fore.YELLOW}[{timestamp}] Tunnel created on port {port} with {ssh_user}@{ssh_host}{Style.RESET_ALL}')
+                    print(f'{Fore.YELLOW}[{timestamp}] Tunnel setting up. Wait...{Style.RESET_ALL}', end='\r')
+                else:
+                    print(f'{Fore.RED}[{timestamp}] Failed to create tunnel.{Style.RESET_ALL}')
+            else:
+                print(f'{Fore.GREEN}[{timestamp}] {Style.BRIGHT}Tunnel is working.({interval}s){Style.RESET_ALL}                   ', end='\r')
+
+            time.sleep(interval)  # Wait for X seconds before checking again
+    except KeyboardInterrupt:
+        timestamp = datetime.now().strftime('%H:%M:%S')
+        print('')
+        kill_tunnel(port)
+        change_network_location('Automatic')
+        print(f'[{timestamp}] {Style.BRIGHT}Bye!{Style.RESET_ALL}')
 
 if __name__ == '__main__':
     timestamp = datetime.now().strftime('%H:%M:%S')
